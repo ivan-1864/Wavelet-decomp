f521ac0bf903da8c87f70044ecfe675088902420
Иван Акимов <ivan@Air-Ivan-6.HomeLAN>
2025-02-02 18:19:39 +0300

    add Haar wavl

--- work/main.m
+++ work/main.m
@@ -3,10 +3,10 @@ TimeCol = 1;
 DataCol = 4;
 
 % number of wavelets on freq-axis     
-j_max = 4;
+j_max = 2;
 
 % number of wavelets on t-axis
-k_max = 25;
+k_max = 20;
 
 % -----------end cfg-----------------------------
 
@@ -20,13 +20,12 @@ clear D
 % mask = find(data(:, 1) >= 16130 & data(:, 1) <= 18549);
 % data = data(mask, :);
 
-
 j_k_max = k_max * 2^j_max;
 
 Time_st = min(Time);
 Time_fin = max(Time);
 
-
+Delta_g  = Hei;
 
 
 % normalize time
@@ -37,40 +36,28 @@ for t = 1 : length(Time)
     shift = 1;
     time = time_wev(t);
     for j = 0 : j_max
-        for k = 0 : k_max * 2^(j_max-j)
+        for k = 0 : k_max * 2^j
           Psi(t, k+shift) = wavel_trf(j, k, time);
         end
-        shift = shift + k_max * 2^(j_max-j);
+        shift = shift + k_max * 2^j;
     end
 end
 
 d = Psi \ Delta_g  ;
 
 Delta_g_app = Psi * d;
-
-% shift = 1;
-% for j = 0 : j_max
-%     k = 0 + shift : k_max * 2^(j_max-j) + shift;
-%     Delta_g_app_lev() = Psi(:, k) * d(k);
-%     figure(j+1)
-%     hold
-%     plot(Time, Delta_g_app_lev)
-%     shift = shift + k_max * 2^(j_max-j);
-% end
+Delta_g_lev = zeros(length(Time), j_max+1);
+shift = 1;
+for j = 0 : j_max
+    index = shift:k_max * 2^j+shift;
+    Delta_g_lev(:, j+1) = Psi(:, index) * d(index);
+    shift = shift + k_max * 2^j;
+end
 
 
-% for j = 0 : j_max
-%     ind = j * (k_max + 1) +1 : (j + 1)  * (k_max + 1); 
-%     Delta_g_part = Psi(:, ind)*d(ind);
-%     figure(1)
-%     hold
-%     plot(Time, Delta_g_part)
-% end
-ind = 401:776;
-Delta_g_app_1 = Psi(:, ind)*d(ind);
-
-figure(10)
+figure(1)
 hold
-plot(Time, Delta_g_app)
-plot(Time, Delta_g_app_1)
 plot(Time, Delta_g)
+plot(Time, Delta_g_app)
+plot(Time, Delta_g_lev)
+legend('true', 'comp', '1', '2', '3')
